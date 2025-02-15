ba32d3f76ec0022936ab08ab7648ab2670044256
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-15 18:46:27 -0500

    Define Task Model

---
+++ models/Task.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Task = sequelize.define(
+  "Task",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    status: {
+      type: DataTypes.ENUM("pending", "completed"),
+      defaultValue: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default Task;
