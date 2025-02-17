c57b3045489c31bf5d5f0e148e6d281d7e0bc61f
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-16 22:40:39 -0500

    Switch then statement with await

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use("/api", taskRoutes);
 const PORT = process.env.PORT || 5000;
 
 // Starting server
-sequelize
-  .sync()
-  .then(() => {
-    console.log("Database synced");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log("Error:", err));
+try {
+  await sequelize.sync();
+  console.log("Database synced");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+} catch (err) {
+  console.log("Error:", err);
+}
